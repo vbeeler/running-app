d61830899597ead41fe7e6509a9eb51d841ba98f
Vanessa Beeler <vbeeler@calpoly.edu>
2022-01-28 12:29:30 -0800

    Initial Commit

---
+++ RunningApp/RunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppApp.swift
+//  RunningApp
+//
+//  Created by Vanessa Beeler on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
