0c920e7818fc1d1d33c464036caf5216d4d74ad3
vbeeler <vanessafbeeler@gmail.com>
2022-02-17 16:52:08 -0800

    Connected Firebase to the app

--- RunningApp/RunningApp.swift
+++ RunningApp/RunningApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct RunningApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
